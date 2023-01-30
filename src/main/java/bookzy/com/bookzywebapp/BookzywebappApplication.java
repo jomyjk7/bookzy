0fdf7fc4ee413682a9591667555b1672638bcfcd
jomyjk7 <jomyjk7@gmail.com>
2023-01-30 11:31:28 +0000

    Added maven project

---
+++ src/main/java/bookzy/com/bookzywebapp/BookzywebappApplication.java
@@ -0,0 +1,13 @@
+package bookzy.com.bookzywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzywebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bookzy/com/bookzywebapp/BookzywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package bookzy.com.bookzywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
