89a69f788158ec738e4bc12bbb1b9a5af6cc7e06
jomyjk7 <jomyjk7@gmail.com>
2023-02-01 11:16:12 +0000

    modified pom and added junit test

--- src/test/java/bookzy/com/bookzywebapp/BookzywebappApplicationTests.java
+++ src/test/java/bookzy/com/bookzywebapp/BookzywebappApplicationTests.java
@@ -1,13 +1,111 @@
-package bookzy.com.bookzywebapp;
+import org.junit.After;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
 
-@SpringBootTest
-class BookzywebappApplicationTests {
+import org.junit.Before;
 
-	@Test
-	void contextLoads() {
-	}
+import org.junit.Ignore;
+
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+/**
+
+ * Created by 
+
+ * Sample junit test code to integrate by
+
+ */
+
+public class JenkinsDemo
+
+{
+
+    private static String Base_Url = "https://www.facebook.com";
+
+    private WebDriver driver;
+
+
+
+    @Before
+
+    public void setUp()
+
+    {
+
+        driver = new ChromeDriver();
+
+        driver.get(Base_Url);
+
+    }
+
+
+
+    @After
+
+    public void after()
+
+    {
+
+        driver.quit();
+
+    }
+
+
+
+    @Test
+
+    public void testCasePassed()
+
+    {
+
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
+
+    }
+
+
+
+    @Test
+
+    public void testCaseFailed()
+
+    {
+
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+
+    }
+
+
+
+    @Ignore
+
+    @Test
+
+    public void testCaseIgnored()
+
+    {
+
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
+
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
